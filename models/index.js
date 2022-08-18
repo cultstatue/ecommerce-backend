19e96eab9fca5d7d20400c37a29ded35fb29a3ba
cultstatue <saltedchurros@gmail.com>
2022-08-18 13:36:50 -0400

    added model associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foriegnKey: 'category_id'
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foriegnKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foriegnKey: 'product_id'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foriegnKey: 'tag_id'
+})
 
 module.exports = {
   Product,
